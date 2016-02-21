f64f2fa3cd81faeeccb94f3e9b9e67d3bc27e457
Danelle Modeste <Danelle.Modeste@gmail.com>
2016-02-21 05:47:40 -0400

    Created List view for Dialogs

--- app/src/main/java/edu/uwi/sta/comp3275a1/Dialogs.java
+++ app/src/main/java/edu/uwi/sta/comp3275a1/Dialogs.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class Dialogs extends AppCompatActivity {
 
@@ -16,14 +18,28 @@ public class Dialogs extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        ListView dialogs=(ListView)findViewById(R.id.dialog_list);
+        dialogs.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position==0){//ALERT
+
+                }
+                else if(position==1){//CONFIRM
+
+                }
+                else if(position==2){//DATE
+
+                }
+                else  if(position==3){//LIST
+
+                }
+                else{//MULTIPLE CHOICE LIST
+
+                }
             }
         });
+
     }
 
 }
