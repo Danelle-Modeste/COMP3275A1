40f69ab7bd5e18ec018865aa647fdb4949310fda
AlanaJDWarden <warden.alana@gmail.com>
2016-02-29 21:23:32 -0400

    Bundle Activity

--- app/src/main/java/edu/uwi/sta/comp3275a1/BundleActivity.java
+++ app/src/main/java/edu/uwi/sta/comp3275a1/BundleActivity.java
@@ -1,8 +1,6 @@
 package edu.uwi.sta.comp3275a1;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -16,14 +14,8 @@ public class BundleActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
 
+    }
 }
+
+
