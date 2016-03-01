5ce3fb1e4d75e88fffae033b69ed533936c4e6ee
AlanaJDWarden <warden.alana@gmail.com>
2016-02-29 22:37:41 -0400

    Bundle Activity and Result Activity 75% completed, Bundle2 was deleted because I realised ResultActivity had to be used instead

--- app/src/main/java/edu/uwi/sta/comp3275a1/BundleActivity.java
+++ app/src/main/java/edu/uwi/sta/comp3275a1/BundleActivity.java
@@ -1,12 +1,18 @@
 package edu.uwi.sta.comp3275a1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class BundleActivity extends AppCompatActivity {
+public class BundleActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private final int BUNDLE2_ACTIVITY_REQUESTCODE=0;
+    private EditText editText;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,7 +20,27 @@ public class BundleActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        editText = (EditText)findViewById(R.id.bundle_edittxt);
+        button = (Button)findViewById(R.id.bundle_btn);
+        button.setOnClickListener(this);
+    }
 
+    public void onClick(View view){
+
+        if(view == button){
+
+            Intent intent = new Intent(getApplicationContext(),ResultActivity.class);
+            intent.putExtra("sendKey",editText.getText().toString());
+            startActivityForResult(intent,BUNDLE2_ACTIVITY_REQUESTCODE);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+
+        super.onActivityResult(requestCode,resultCode,intent);
+        Bundle extras = intent.getExtras();
+
+        editText.setText(extras!=null ? extras.getString("returnKey"):"nothing returned");
     }
 }
 
--- app/src/main/java/edu/uwi/sta/comp3275a1/ResultActivity.java
+++ app/src/main/java/edu/uwi/sta/comp3275a1/ResultActivity.java
@@ -1,13 +1,18 @@
 package edu.uwi.sta.comp3275a1;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button button;
+    private String intentString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +21,28 @@ public class ResultActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        editText = (EditText)findViewById(R.id.result_edittxt);
+        button = (Button)findViewById(R.id.result_btn);
+
+        button.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view){
+
+                intentString = editText.getText().toString();
+
+                Intent intent = new Intent();
+                intent.putExtra("returnKey",editText.getText().toString());
+                setResult(RESULT_OK,intent);
+                finish();
             }
         });
+
+        intentString = savedInstanceState!=null ? savedInstanceState.getString("sendKey"):null;
+        if(intentString == null){
+            Bundle extras = getIntent().getExtras();
+            intentString = extras!=null ? extras.getString("sendKey"): "nothing passed in";
+        }
+        editText.setText(intentString);
     }
 
 }
