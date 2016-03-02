8270663a4c67b8df17c80c53095e8b684d51e0b7
Danelle Modeste <Danelle.Modeste@gmail.com>
2016-03-02 02:30:12 -0400

    Update

--- app/src/main/java/edu/uwi/sta/comp3275a1/CustomAdapter.java
+++ app/src/main/java/edu/uwi/sta/comp3275a1/CustomAdapter.java
@@ -1,5 +1,6 @@
 package edu.uwi.sta.comp3275a1;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,9 +10,15 @@ import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CustomAdapter extends AppCompatActivity {
-    ListView itemList;
-    adapter customAdapter;
+    ListView itemList; //List View of items
+    adapter customAdapter; //Custom adapter for list items in the list view
+    private ArrayList<Map> items; //List of item information to be displayed in list view
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,23 +26,28 @@ public class CustomAdapter extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       /* itemList=(ListView)findViewById(R.id.listView);
-        //customAdapter=new adapter(this)
-        String[] menuList = { "HOME", "PREFERENCES", "VIEW MAP","LOG OUT"};
-        Integer[] icons = {R.drawable.home2,R.drawable.settings2,R.drawable.view2,R.drawable.logout2};
+        //gets the list view from the xml
+        itemList=(ListView)findViewById(R.id.listView);
 
-        CustomListAdapter Adapter= new CustomListAdapter(this, menuList, icons);
-        DrawerList.setAdapter(Adapter);*/
-    }
+        items = new ArrayList<>();
 
-    public void ShowDesc(View view){
-        TextView tv =(TextView)findViewById(R.id.desc);
-        if(tv.getVisibility()==View.VISIBLE){
-            tv.setVisibility(View.INVISIBLE);
+        //Retrieves item information to be displayed in list view
+        String[] title = getResources().getStringArray(R.array.items_available);
+        String[] description= getResources().getStringArray(R.array.items_description);
+        TypedArray icons =getResources().obtainTypedArray(R.array.items_images);
+
+        int icon;
+        //Sends item information for each item to be displayed in the list view
+        for(int i=0;i<title.length;i++){
+            icon=icons.getResourceId(i,-1);
+            Map temp = new HashMap();
+            temp.put("icons",icon);
+            temp.put("title",title[i]);
+            temp.put("description",description[i]);
+            items.add(temp);
         }
-        else{
-            tv.setVisibility(View.VISIBLE);
-        }
+        customAdapter= new adapter(this, items);//creates an instance of a custom adapter with item information
+        itemList.setAdapter(customAdapter); //sets the adapter to the item list
     }
 }
 
--- app/src/main/java/edu/uwi/sta/comp3275a1/adapter.java
+++ app/src/main/java/edu/uwi/sta/comp3275a1/adapter.java
@@ -1,7 +1,58 @@
 package edu.uwi.sta.comp3275a1;
 
-/**
- * Created by Alana Warden on 02/03/2016.
- */
-public class adapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.Map;
+
+//Custom Adapter to display Item information in list view
+public class adapter extends ArrayAdapter<Map> {
+
+    public adapter(Context context, ArrayList<Map> items){
+        super(context, 0, items);
+    }
+
+    public View getView(int position, View v, ViewGroup p){
+        //Retrieves the view item position and View group
+        v = LayoutInflater.from(getContext()).inflate(R.layout.list_item, null);
+        final View V = v; // Copies the view into a final variable to be used by button function
+
+        //Retrieves position, title, description and image information from the map
+        Map map = getItem(position);
+
+        String title = (String)map.get("title");
+        String descr = (String)map.get("description");
+        int icon = (Integer)map.get("icons");
+
+        //Displays the information in the relevant areas in the view
+        ((TextView)v.findViewById(R.id.title)).setText(title);
+        ((TextView)v.findViewById(R.id.desc)).setText(descr);
+        ((ImageView)v.findViewById(R.id.img)).setImageResource(icon);
+
+        //Retrieves  the button from the view and sets the function to it
+        final Button showDesc = (Button) v.findViewById(R.id.showDesc);
+        showDesc.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+                TextView tv =(TextView) V.findViewById(R.id.desc);
+                if(tv.getVisibility()==View.VISIBLE){
+                    tv.setVisibility(View.INVISIBLE);
+                    showDesc.setText("Show Description");
+                }
+                else{
+                    tv.setVisibility(View.VISIBLE);
+                    showDesc.setText("Hide Description");
+                }
+            }
+        });
+        return v; //Returns the view to be displayed as a list view item
+    }
 }
+
